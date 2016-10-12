0b2eba8b5ded1d86dba58b6013dc54a17169b9be
Wiekhorst <florian.wiekhorst@haw-hamburg.de>
2016-10-12 15:40:00 +0200

    added Hast-du-Fachabitur

--- Studienberatung.java
+++ Studienberatung.java
@@ -5,5 +5,13 @@ public class Studienberatung{
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hast du Abitur?");
 		String antwort = scanner.next();
+		if (antwort.equalsIgnoreCase("n"){
+			
+			System.out.println("Hast du Fachabitur?");
+			String antwort = scanner.next();
+		}
+		else{
+			
+		}
 	}
 }
