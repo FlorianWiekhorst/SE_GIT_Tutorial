dff428abb4e1bc0e49a4b77c952a150938071ae7
Wiekhorst <florian.wiekhorst@haw-hamburg.de>
2016-10-12 15:45:17 +0200

    added wissbegierig

--- Studienberatung.java
+++ Studienberatung.java
@@ -5,5 +5,14 @@ public class Studienberatung{
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hast du Abitur?");
 		String antwort = scanner.next();
+		if (antwort.equalsIgnoreCase("n"){
+			
+			System.out.println("Hast du Fachabitur?");
+			String antwort = scanner.next();
+		}
+		else{
+			System.out.println("Bist du wissbegierig?");
+			String antwort = scanner.next();
+		}
 	}
 }
